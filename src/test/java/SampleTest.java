00be586c59bdd1e107e53f5b2eb0e0ca07475e8d
Gloria-HH <zhihuilvy@gmail.com>
2019-05-03 17:39:26 +0800

    fix code

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -15,37 +15,37 @@ public class SampleTest {
 
     private   AndroidDriver driver;
 
-    @BeforeEach
-    public  void setUp() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("deviceName", "ddd");
-        desiredCapabilities.setCapability("platformName", "android");
-        desiredCapabilities.setCapability("appPackage", "com.xueqiu.android");
-        desiredCapabilities.setCapability("appActivity", ".view.WelcomeActivityAlias");
-//        desiredCapabilities.setCapability("appPackage", "io.appium.settings");
-//        desiredCapabilities.setCapability("appActivity", ".Settings");
-        desiredCapabilities.setCapability("autoGrantPermissions", true);
-
-        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
-
-        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
-    }
-
-    @Test
-    public void swipe(){
-        try {
-            for(int i=0;i<10;i++){
-                FileUtils.copyFile(driver.getScreenshotAs(OutputType.FILE).getCanonicalFile(),new File(i+".png"));
-            }
-
-        } catch (IOException e) {
-
-        }
-    }
-
-    @AfterEach
-    public  void tearDown() {
-        driver.quit();
-    }
+//    @BeforeEach
+//    public  void setUp() throws MalformedURLException {
+//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setCapability("deviceName", "ddd");
+//        desiredCapabilities.setCapability("platformName", "android");
+//        desiredCapabilities.setCapability("appPackage", "com.xueqiu.android");
+//        desiredCapabilities.setCapability("appActivity", ".view.WelcomeActivityAlias");
+////        desiredCapabilities.setCapability("appPackage", "io.appium.settings");
+////        desiredCapabilities.setCapability("appActivity", ".Settings");
+//        desiredCapabilities.setCapability("autoGrantPermissions", true);
+//
+//        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+//
+//        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+//    }
+//
+//    @Test
+//    public void swipe(){
+//        try {
+//            for(int i=0;i<10;i++){
+//                FileUtils.copyFile(driver.getScreenshotAs(OutputType.FILE).getCanonicalFile(),new File(i+".png"));
+//            }
+//
+//        } catch (IOException e) {
+//
+//        }
+//    }
+//
+//    @AfterEach
+//    public  void tearDown() {
+//        driver.quit();
+//    }
 }
 
